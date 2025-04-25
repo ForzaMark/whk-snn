4599bef8fe55e10b3647a1cf7326e217bef8f4be
Mark Heimer <me@cratory.de>
2025-04-25 16:42:43 +0200

    implement all train test splits

--- heidelberg_implementation/train_simplified_snn.py
+++ heidelberg_implementation/train_simplified_snn.py
@@ -10,6 +10,25 @@ from utils import get_device
 import numpy as np
 import matplotlib.pyplot as plt
 
+def compute_test_set_accuracy(test_data_generator, net):
+    total = 0
+    correct = 0
+
+    with torch.no_grad():
+        net.eval()
+        for data, targets in test_data_generator:
+            data = data.to_dense().permute(1, 0, 2).to(device)
+            targets = targets.to(device)
+
+            test_spk, _ = net(data)
+
+            _, predicted = test_spk.sum(dim=0).max(1)
+            total += targets.size(0)
+            correct += (predicted == targets).sum().item()
+
+
+    return 100 * correct / total
+
 time_steps = 100
 num_inputs = 700
 num_hidden = 1000
@@ -54,8 +73,15 @@ class Net(nn.Module):
 if __name__ == "__main__":
     x_train, y_train, x_test, y_test = get_train_test_data()    
 
-    data_generator = list(sparse_data_generator_from_hdf5_spikes(x_train, y_train,
+    train_data_generator = list(sparse_data_generator_from_hdf5_spikes(x_train, y_train,
                                        batch_size=batch_size,
+                                       nb_steps=time_steps,
+                                       nb_units=num_inputs,
+                                       max_time=max_time,
+                                       device=device))
+    
+    test_data_generator = list(sparse_data_generator_from_hdf5_spikes(x_test, y_test,
+                                       batch_size=1,
                                        nb_steps=time_steps,
                                        nb_units=num_inputs,
                                        max_time=max_time,
@@ -68,7 +94,7 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(net.parameters(), lr=5e-4, betas=(0.9, 0.999))
     loss = nn.CrossEntropyLoss()
 
-    num_epochs = 3
+    num_epochs = 1
 
     global_loss_hist = []
     global_acc_hist = []
@@ -76,14 +102,14 @@ if __name__ == "__main__":
     for epoch in range(num_epochs):
         print(f"Epoch: {epoch}")
 
-        for i, (data, targets) in enumerate(data_generator):
+        for i, (data, targets) in enumerate(train_data_generator):
             loss_hist = []
             acc_hist = []
 
             data = data.to_dense().permute(1, 0, 2).to(device)
             targets = targets.to(device)
 
-            spk_rec, mem_rec = net.forward(data)
+            spk_rec, mem_rec = net(data)
             loss_val = torch.zeros((1), dtype=dtype, device=device)
 
             # sum loss at every step
@@ -107,3 +133,7 @@ if __name__ == "__main__":
 
     save_history_plot(global_loss_hist, name='simplified_loss')
     save_history_plot(global_acc_hist, name='simplified_accuracy')
+
+    test_set_accuracy = compute_test_set_accuracy(test_data_generator, net)
+    print(f"Test Set Accuracy: {test_set_accuracy:.2f}%")
+
