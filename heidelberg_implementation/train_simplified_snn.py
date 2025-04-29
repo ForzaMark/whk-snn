2eeada21f693a503fa8fc0e9b1304f370979432d
Mark Heimer <me@cratory.de>
2025-04-29 11:53:16 +0200

    refactoring simplified

--- heidelberg_implementation/train_simplified_snn.py
+++ heidelberg_implementation/train_simplified_snn.py
@@ -1,14 +1,12 @@
-import snntorch as snn
-from snntorch import surrogate
 from snntorch import functional as SF
-from snntorch import spikeplot as splt
-from snntorch import utils
 import torch.nn as nn
-from utils import sparse_data_generator_from_hdf5_spikes, get_train_test_data, save_history_plot
+from utils import save_history_plot
 import torch
 from utils import get_device
 import numpy as np
-import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from tonic import datasets, transforms
+from single_hidden_layer_1000_neurons_net import SingleHiddenLayer1000NeuronsNet
 
 def compute_test_set_accuracy(test_data_generator, net):
     total = 0
@@ -17,7 +15,7 @@ def compute_test_set_accuracy(test_data_generator, net):
     with torch.no_grad():
         net.eval()
         for data, targets in test_data_generator:
-            data = data.to_dense().permute(1, 0, 2).to(device)
+            data = data.to_dense().to(torch.float32).squeeze().permute(1, 0, 2).to(device)
             targets = targets.to(device)
 
             test_spk, _ = net(data)
@@ -28,6 +26,21 @@ def compute_test_set_accuracy(test_data_generator, net):
 
 
     return 100 * correct / total
+
+def calculate_loss_over_all_timesteps(time_steps, mem_rec, targets, loss):
+    loss_val = torch.zeros((1), dtype=dtype, device=device)
+
+    for step in range(time_steps):
+        loss_val += loss(mem_rec[step], targets)
+
+    return loss_val
+
+def calculate_gradient(optimizer, loss_val):
+    optimizer.zero_grad()
+    loss_val.backward()
+
+def update_weights(optimizer):
+    optimizer.step()
 
 time_steps = 100
 num_inputs = 700
@@ -40,56 +53,21 @@ max_time = 1.4
 beta = 0.99
 device = get_device()
 
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        # Initialize layers
-        self.fc1 = nn.Linear(num_inputs, num_hidden)
-        self.lif1 = snn.Leaky(beta=beta)
-        self.fc2 = nn.Linear(num_hidden, num_outputs)
-        self.lif2 = snn.Leaky(beta=beta)
-
-    def forward(self, x):
-
-        # Initialize hidden states at t=0
-        mem1 = self.lif1.init_leaky()
-        mem2 = self.lif2.init_leaky()
-
-        # Record the final layer
-        spk2_rec = []
-        mem2_rec = []
-
-        for step in range(time_steps):
-            cur1 = self.fc1(x[step])
-            spk1, mem1 = self.lif1(cur1, mem1)
-            cur2 = self.fc2(spk1)
-            spk2, mem2 = self.lif2(cur2, mem2)
-            spk2_rec.append(spk2)
-            mem2_rec.append(mem2)
-
-        return torch.stack(spk2_rec, dim=0), torch.stack(mem2_rec, dim=0)
+frame_transform = transforms.ToFrame(
+    sensor_size=datasets.SHD.sensor_size,  
+    n_time_bins=time_steps
+)
 
 if __name__ == "__main__":
-    x_train, y_train, x_test, y_test = get_train_test_data()    
+    print('prepare data')
 
-    train_data_generator = list(sparse_data_generator_from_hdf5_spikes(x_train, y_train,
-                                       batch_size=batch_size,
-                                       nb_steps=time_steps,
-                                       nb_units=num_inputs,
-                                       max_time=max_time,
-                                       device=device))
+    train_data = datasets.SHD("./data", transform=frame_transform, train=True)
+    test_data = datasets.SHD("./data", transform=frame_transform, train=False)
     
-    test_data_generator = list(sparse_data_generator_from_hdf5_spikes(x_test, y_test,
-                                       batch_size=1,
-                                       nb_steps=time_steps,
-                                       nb_units=num_inputs,
-                                       max_time=max_time,
-                                       device=device))
+    train_data_loader = DataLoader(train_data, shuffle=False, batch_size=batch_size)
+    test_data_loader = DataLoader(test_data, shuffle=False, batch_size=batch_size)
     
-    print("data generated")
-    
-    net = Net().to(device)
+    net = SingleHiddenLayer1000NeuronsNet(num_inputs=num_inputs, num_outputs=num_outputs, beta=beta, time_steps=time_steps).to(device)
 
     optimizer = torch.optim.Adam(net.parameters(), lr=5e-4, betas=(0.9, 0.999))
     loss = nn.CrossEntropyLoss()
@@ -102,38 +80,38 @@ if __name__ == "__main__":
     for epoch in range(num_epochs):
         print(f"Epoch: {epoch}")
 
-        for i, (data, targets) in enumerate(train_data_generator):
+        for i, (data, targets) in enumerate(train_data_loader):
             loss_hist = []
             acc_hist = []
 
-            data = data.to_dense().permute(1, 0, 2).to(device)
+            data = data.to_dense().to(torch.float32).squeeze().permute(1, 0, 2).to(device)
             targets = targets.to(device)
 
             spk_rec, mem_rec = net(data)
-            loss_val = torch.zeros((1), dtype=dtype, device=device)
 
-            # sum loss at every step
-            for step in range(time_steps):
-                loss_val += loss(mem_rec[step], targets)
+            loss_val = calculate_loss_over_all_timesteps(time_steps, mem_rec, targets, loss)
 
-            # Gradient calculation + weight update
-            optimizer.zero_grad()
-            loss_val.backward()
-            optimizer.step()
-
+            calculate_gradient(optimizer=optimizer, loss_val=loss_val)
+            update_weights(optimizer=optimizer)
+            
             acc = SF.accuracy_rate(spk_rec, targets)
-
-            global_loss_hist.append(loss_val.item())
-            global_acc_hist.append(acc)
-            loss_hist.append(loss_val.item())
             acc_hist.append(acc)
             
-        print(f"average loss {np.array(loss_hist).mean()}")
-        print(f"average accuracy {np.array(acc_hist).mean()}")
+        print(f"loss {loss_val.item()}")
+        print(f"accuracy {np.array(acc_hist).mean()}")
+        
+        global_loss_hist.append(loss_val.item())
+        global_acc_hist.append(np.array(acc_hist).mean())
 
     save_history_plot(global_loss_hist, name='simplified_loss')
     save_history_plot(global_acc_hist, name='simplified_accuracy')
 
-    test_set_accuracy = compute_test_set_accuracy(test_data_generator, net)
-    print(f"Test Set Accuracy: {test_set_accuracy:.2f}%")
-
+    test_set_accuracy = compute_test_set_accuracy(test_data_loader, net)
+    output = f"""
+        Epochs = {num_epochs}
+        Training Accuracy = {global_acc_hist[len(global_acc_hist) - 1]*100:.2f}%
+        Test Accuracy = {test_set_accuracy:.2f}%
+    """
+    with open("output/simplified_results.txt", "w") as file:
+        file.write(output)
+    
